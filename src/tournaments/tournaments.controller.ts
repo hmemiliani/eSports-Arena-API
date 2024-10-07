f36e90a804930a4e4c6a62d41b348746f6aab47a
hmemiliani <harold_medranoe@hotmail.com>
2024-10-07 15:55:48 -0500

    feat(tournament): add tournament controller

--- src/tournaments/tournaments.controller.ts
+++ src/tournaments/tournaments.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { TournamentsService } from './tournaments.service';
+import { Tournament } from './entities/tournament.entity';
 
 @Controller('tournaments')
-export class TournamentsController {}
+export class TournamentsController {
+  constructor(private readonly tournamentsService: TournamentsService) {}
+
+  @Get()
+  findAll() {
+    return this.tournamentsService.findAll();
+  }
+
+  @Post()
+  create(@Body() tournament: Tournament) {
+    return this.tournamentsService.create(tournament);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.tournamentsService.findOneById(+id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.tournamentsService.remove(+id);
+  }
+}
